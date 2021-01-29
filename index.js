352171bb824610ab7abb85a086a31a86017c5ca2
Aarav Agarwal <aaravagarwal@Aaravs-MacBook-Pro.local>
2021-01-29 18:32:12 -0500

    Database connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config');
 
 const typeDefs = gql`
 	type Query {
@@ -18,7 +20,11 @@ const server = new ApolloServer({
 	resolvers
 })
 
-server.listen({ port: 5000 })
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Database Connected!")
+		return server.listen({ port: 5000 })
+	})
 	.then((res) => {
 		console.log(`Server running at ${res.url}`)
 	})
